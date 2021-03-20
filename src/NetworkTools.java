27c4eecaf468a604e45b67ca27a17703111f48df
karibov@btlab.ru <12345678>
2021-03-20 13:37:51 +0300

    Delete methods that never been used

--- src/NetworkTools.java
+++ src/NetworkTools.java
@@ -38,47 +38,4 @@ public class NetworkTools {
         return Math.random()*(upper_bound-lower_bound) + lower_bound;
     }
 
-    public static Integer[] randomValues(int lowerBound, int upperBound, int amount) {
-
-        lowerBound --;
-
-        if(amount > (upperBound-lowerBound)){
-            return null;
-        }
-
-        Integer[] values = new Integer[amount];
-        for(int i = 0; i< amount; i++){
-            int n = (int)(Math.random() * (upperBound-lowerBound+1) + lowerBound);
-            while(containsValue(values, n)){
-                n = (int)(Math.random() * (upperBound-lowerBound+1) + lowerBound);
-            }
-            values[i] = n;
-        }
-        return values;
-    }
-
-    public static <T extends Comparable<T>> boolean containsValue(T[] ar, T value){
-        for(int i = 0; i < ar.length; i++){
-            if(ar[i] != null){
-                if(value.compareTo(ar[i]) == 0){
-                    return true;
-                }
-            }
-
-        }
-        return false;
-    }
-
-    public static int indexOfHighestValue(double[] values){
-        int index = 0;
-        for(int i = 1; i < values.length; i++){
-            if(values[i] > values[index]){
-                index = i;
-            }
-        }
-        return index;
-    }
-
-
-
 }
